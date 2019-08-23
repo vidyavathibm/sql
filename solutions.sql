97160bba70b1ae98a03a2a8c1e85cc6534d3af22
vidyavathibm <38814597+vidyavathibm@users.noreply.github.com>
2019-08-23 11:16:03 +0530

    Update solutions.sql

--- solutions.sql
+++ solutions.sql
@@ -1,4 +1,4 @@
-create Database Empoloyee_dept_Db;
+9create Database Empoloyee_dept_Db;
 use Empoloyee_dept_Db;
 
 
@@ -48,11 +48,83 @@ select * from (select name,salary,dense_rank() over (order by salary asc) as r
 from employee) t where r=1;
 
 #3)Select the Employee who does not have a manager in the department table
-select e.e_id,e.name,e.managerid,e.dep_id,t1.manager_name from employee e inner join 
+select * from employee e left join 
 (select e1.e_id,e1.name,e1.managerid,e1.dep_id,e2.name "manager_name" from employee e1 
-join employee e2 on e1.managerid=e2.e_id) as t1 on e.dep_id=t1.dep_id where t1.managerid is null;
+left join employee e2 on e1.managerid=e2.e_id) as t1 on e.dep_id=t1.dep_id where t1.managerid is null;
 
-select e.name from employee e inner join dept d on e.dep_id=d.dep_id where d.depmanager="Tim Cook"
+#4)Select the Employee who is also a Manager
+select e.name from employee e inner join dept d on e.name=d.depmanager;
 
 
+#5)Select the Empolyee who is a Manager and has least salary
+select * from (select e.name,e.salary,dense_rank() over(order by e.salary) as d
+ from employee e inner join dept d on e.name=d.depmanager) as t where d=1;
 
+#6)Select the total number of Employees in Communications departments
+ select d.depname,count(e.e_id) from employee e inner join dept d on e.dep_id=d.dep_id 
+ where depname="Communications";
+ 
+#7)Select the Employee in Finance Department who has the top salary
+ select * from (select e.name,e.salary,dense_rank() over (order by salary desc) as d 
+ from employee e 
+ inner join dept d on e.dep_id=d.dep_id where depname="Finance")as t where d=1;
+
+#8)Select the Employee in product depatment who has the least salary
+ select * from (select e.name,e.salary,dense_rank() over (order by salary) as d 
+ from employee e 
+ inner join dept d on e.dep_id=d.dep_id where depname="Product")as t where d=1;
+
+#9)Select the count of Empolyees in Health with maximum salary
+select * from (select e.name,d.depname,e.salary,count(e.e_id),dense_rank() over (order by salary desc) as d 
+ from employee e inner join dept d 
+ on e.dep_id=d.dep_id 
+ where depname="Health") t where d=1;
+ 
+#10)Select the Employees who report to Natasha Stevens
+select e.name,d.depmanager from employee e inner join dept d on e.dep_id=d.dep_id 
+where d.depmanager="Natasha Stevens";
+
+#11)Display the Employee name,Employee count,Dep name,Dept manager in the Health department
+select e.name,count(e_id),d.depname,d.depmanager from employee e 
+inner join dept d on e.dep_id=d.dep_id 
+where d.depname="Health";
+
+#12)Display the Department id,Employee ids and Manager ids for the Communications department
+select d.dep_id,e.e_id,e.managerid,d.depname from employee e 
+inner join dept d on e.dep_id=d.dep_id 
+where d.depname="Communications";
+
+#13)Select the Average Expenses for Each dept with Dept id and Dept name
+select d.depname,avg(e.salary) from employee e inner join dept d  on e.dep_id=d.dep_id 
+group by d.dep_id,d.depname;
+
+#14)Select the total expense for the department finance
+select d.depname,sum(e.salary) from employee e 
+inner join dept d on e.dep_id=d.dep_id 
+where d.depname="Finance";
+
+#15)Select the department which spends the least with Dept id and Dept manager name
+
+select * from (select d.depname,sum(salary),dense_rank() over (order by sum(salary)) as d from employee e inner join dept d 
+on e.dep_id=d.dep_id group by d.dep_id,d.depmanager) as t where d=1;
+
+#16)Select the count of Employees in each department
+select d.depname,count(e_id) from employee e inner join dept d on e.dep_id=d.dep_id 
+group by d.depname;
+
+#17)Select the count of Employees in each department having salary <10000
+select d.depname,count(e_id) from employee e inner join dept d on e.dep_id=d.dep_id where 
+e.salary<20000 group by d.depname;
+
+#18)Select the total number of Employees in Dept id D04
+select count(e_id) from employee e inner join dept d on e.dep_id=d.dep_id where d.dep_id='D04';
+
+#19)Select all department details of the Department with Maximum Employees
+select * from (select d.depname,count(e_id),dense_rank() over 
+(order by count(e_id) desc) as d from employee e inner join 
+dept d on e.dep_id=d.dep_id group by d.depname) as t where d=1;
+
+
+#20)Select the Employees who has Tim Cook as their manager
+select e.name from employee e inner join dept d on e.dep_id=d.dep_id
+ where d.depmanager="Tim Cook"
