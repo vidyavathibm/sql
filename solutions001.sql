107f98cc78e6ea322e3317e6c25e3af36816b377
vidyavathibm <38814597+vidyavathibm@users.noreply.github.com>
2019-08-23 12:18:03 +0530

    Update solutions001.sql

--- solutions001.sql
+++ solutions001.sql
@@ -2,22 +2,23 @@ select * from guest;
 select * from booking;
 select * from hotel;
 select * from room;
+create Database hotel_db;
+use hotel_db;
 
 #1.List full details of all hotels.
-select t2.hotel_no,t2.name,t2.city,t2.room_no,t2.rtype,t2.price,t2.guest_no,t2.date_from,t2.date_to,g.name,g.city
-from (select t1.hotel_no,t1.name,t1.city,t1.room_no,t1.rtype,t1.price,b.guest_no,b.date_from,b.date_to
-from (select h.hotel_no,h.name,h.city,r.room_no,r.rtype,r.price from hotel h 
-left join room r on h.hotel_no=r.hotel_no) t1 
-left join booking b on t1.hotel_no=b.hotel_no and t1.room_no=b.room_no) t2 
-left join guest g on t2.guest_no=g.guest_no;
+select h.hotel_no,h.name,h.city,r.room_no,r.rtype,r.price,
+g.guest_no,b.date_from,b.date_to,g.name,g.city from hotel h 
+left join room r on h.hotel_no=r.hotel_no
+left join booking b on h.hotel_no=b.hotel_no and r.room_no=b.room_no
+left join guest g on b.guest_no=g.guest_no;
 
 #2.List full details of all hotels in New York.
-select t2.hotel_no,t2.name,t2.city,t2.room_no,t2.rtype,t2.price,t2.guest_no,t2.date_from,t2.date_to,g.name,g.city
-from (select t1.hotel_no,t1.name,t1.city,t1.room_no,t1.rtype,t1.price,b.guest_no,b.date_from,b.date_to
-from (select h.hotel_no,h.name,h.city,r.room_no,r.rtype,r.price from hotel h 
-left join room r on h.hotel_no=r.hotel_no where h.city="New York") t1 
-left join booking b on t1.hotel_no=b.hotel_no and t1.room_no=b.room_no) t2 
-left join guest g on t2.guest_no=g.guest_no;
+
+select h.hotel_no,h.name,h.city,r.room_no,r.rtype,r.price,
+g.guest_no,b.date_from,b.date_to,g.name,g.city from hotel h 
+left join room r on h.hotel_no=r.hotel_no
+left join booking b on h.hotel_no=b.hotel_no and r.room_no=b.room_no
+left join guest g on b.guest_no=g.guest_no where h.city="New York";
 
 #3.List the names and cities of all guests, ordered according to their cities.
 select name,city from guest order by city;
@@ -38,8 +39,8 @@ select avg(price) from room;
 select h.name as Hotel_name,r.room_no,r.rtype from hotel h inner join room r on h.hotel_No=r.hotel_No;
 
 #9.List the hotel names, booking dates, and room numbers for all hotels in New York.
-select h.name as Hotel_name,b.room_no,b.Date_From,b.Date_To from booking b 
-inner join hotel h on b.hotel_No=h.hotel_No;
+select h.name as Hotel_name,b.room_no,b.Date_From as "Booking dates" from booking b 
+inner join hotel h on b.hotel_No=h.hotel_No where h.city="New York";
 
 #10.What is the number of bookings that started in the month of September?
 select count(*) as bookings_in_september from booking where month(Date_From)=9;
